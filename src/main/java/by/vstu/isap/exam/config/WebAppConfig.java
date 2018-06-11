7c6b52506263586f23b4eef65470a473fed88d6c
Homa <Homa@Book>
2018-06-11 22:11:07 +0300

    b2

--- src/main/java/by/vstu/isap/exam/config/WebAppConfig.java
+++ src/main/java/by/vstu/isap/exam/config/WebAppConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 @EnableWebMvc
 @ImportResource("classpath:spring-jpa.xml")
-// boot
+// boot   
 @ComponentScan("by.vstu.isap.exam")
 public class WebAppConfig extends WebMvcConfigurerAdapter {
 
