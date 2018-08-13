f65a71bb7f154d310d670c17e9ef498112beb7b4
Homa <Homa@Book>
2018-08-13 20:21:06 +0300

    Lab4 Angular Services CORS

--- src/main/java/by/vstu/isap/exam/config/WebAppConfig.java
+++ src/main/java/by/vstu/isap/exam/config/WebAppConfig.java
@@ -3,6 +3,7 @@ package by.vstu.isap.exam.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -12,5 +13,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 // boot   
 @ComponentScan("by.vstu.isap.exam")
 public class WebAppConfig extends WebMvcConfigurerAdapter {
-
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 }
